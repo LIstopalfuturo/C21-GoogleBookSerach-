8b64a8873437aa4de3caef13f8fcc4cc10f1c193
Alberto C. Garcia <albertogarcia767@yahoo.com>
2025-01-28 13:15:03 -0500

    changed package.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -17,6 +17,11 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 })
