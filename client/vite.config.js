3c434f89bd4388549a071adf1c978feb89d8b19e
Alberto C. Garcia <albertogarcia767@yahoo.com>
2025-01-28 19:31:12 -0500

    saved

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react': 'react',
+      'react-dom': 'react-dom'
+    }
+  },
   server: {
     port: 3000,
     open: true,
     proxy: {
       '/graphql': {
-        target: process.env.NODE_ENV === 'development' 
-          ? 'http://localhost:3001'
-          : '/',
+        target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
       }
