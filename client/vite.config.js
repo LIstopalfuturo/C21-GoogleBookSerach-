9325ec63df525b0c4b410c454e45f9d1c51e846a
Alberto C. Garcia <albertogarcia767@yahoo.com>
2025-01-28 19:20:50 -0500

    saved

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,9 @@ export default defineConfig({
     open: true,
     proxy: {
       '/graphql': {
-        target: 'http://localhost:3001',
+        target: process.env.NODE_ENV === 'development' 
+          ? 'http://localhost:3001'
+          : '/',
         changeOrigin: true,
         secure: false,
       }
